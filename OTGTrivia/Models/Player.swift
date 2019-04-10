0fb61a5d8766480fb60f0c53ad8484507d0831b0
Julio Reyes <Julio.Reyes@queenslibrary.org>
2019-04-10 17:43:10 -0400

    Forgot to add NSCoding protocols to the Player model. Caused a crash towards saving.

--- OTGTrivia/Models/Player.swift
+++ OTGTrivia/Models/Player.swift
@@ -8,24 +8,127 @@
 
 import UIKit
 
-class Player: NSObject, Codable {
-    var playerID: Int
-    var playerName: String
-    var playerScore: Int
-    var difficulty: String
-    var gameType: String
+class Player: NSObject, NSCoding {
     
-    var correctAnswers: Int
-    var incorrectAnswers: Int
+    struct Keys{
+        static let playerID = "ID"
+        static let playerName = "Name"
+        static let playerScore = "Score"
+        static let difficulty = "Difficulty"
+        static let gameType = "Game Type"
+        static let correctAnswers = "Correct"
+        static let incorrectAnswers = "Incorrect"
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(_playerID, forKey: Keys.playerID)
+        aCoder.encode(_playerName, forKey: Keys.playerName)
+        
+        aCoder.encode(_playerScore, forKey: Keys.playerScore)
+        aCoder.encode(_difficulty, forKey: Keys.difficulty)
+        aCoder.encode(_gameType, forKey: Keys.gameType)
+        aCoder.encode(_correctAnswers, forKey: Keys.correctAnswers)
+        aCoder.encode(_incorrectAnswers, forKey: Keys.incorrectAnswers)
+    }
+    
+    override init() { }
+    required init?(coder aDecoder: NSCoder) {
+        if let playerIDObj = aDecoder.decodeObject(forKey: Keys.playerID) as? Int {
+            _playerID = playerIDObj
+        }
+        if let playerNameObj = aDecoder.decodeObject(forKey: Keys.playerName) as? String {
+            _playerName = playerNameObj
+        }
+        if let playerScoreObj = aDecoder.decodeObject(forKey: Keys.playerScore) as? Int {
+            _playerScore = playerScoreObj
+        }
+        if let difficultyObj = aDecoder.decodeObject(forKey: Keys.difficulty) as? String {
+            _difficulty = difficultyObj
+        }
+        if let gameTypeObj = aDecoder.decodeObject(forKey: Keys.gameType) as? String {
+            _gameType = gameTypeObj
+        }
+        if let correctAnswersObj = aDecoder.decodeObject(forKey: Keys.correctAnswers) as? Int {
+            _correctAnswers = correctAnswersObj
+        }
+        if let incorrectAnswersObj = aDecoder.decodeObject(forKey: Keys.incorrectAnswers) as? Int {
+            _incorrectAnswers = incorrectAnswersObj
+        }
+    }
+    
+    private var _playerID: Int = 0
+    private var _playerName: String = ""
+    private var _playerScore: Int = 0
+    private var _difficulty: String  = ""
+    private var _gameType: String  = ""
+    
+    private var _correctAnswers: Int = 0
+    private var _incorrectAnswers: Int = 0
     
     init(id: Int, playerName: String, playerScore: Int, difficulty: String,
          gameType: String, correctAnswers: Int, incorrectAnswers: Int) {
-        self.playerID = id
-        self.playerName = playerName
-        self.playerScore = playerScore
-        self.difficulty = difficulty
-        self.gameType = gameType
-        self.correctAnswers = correctAnswers
-        self.incorrectAnswers = incorrectAnswers
+        self._playerID = id
+        self._playerName = playerName
+        self._playerScore = playerScore
+        self._difficulty = difficulty
+        self._gameType = gameType
+        self._correctAnswers = correctAnswers
+        self._incorrectAnswers = incorrectAnswers
+    }
+    
+    var playerID: Int {
+        get{
+            return _playerID
+        }set{
+            _playerID = newValue
+        }
+    }
+    var playerName: String {
+        get{
+            return _playerName
+        }set{
+            _playerName = newValue
+        }
+    }
+    var playerScore: Int {
+        get{
+            return _playerScore
+        }set{
+            _playerScore = newValue
+        }
+    }
+    var difficulty: String {
+        get{
+            return _difficulty
+            
+        }set{
+            _difficulty = newValue
+            
+        }
+    }
+    var gameType: String {
+        get{
+            return _gameType
+            
+        }set{
+            _gameType  = newValue
+        }
+    }
+    
+    var correctAnswers: Int  {
+        get{
+            return _correctAnswers
+            
+        }set{
+            _correctAnswers = newValue
+            
+        }
+    }
+    var incorrectAnswers: Int  {
+        get{
+            return _incorrectAnswers
+        }set{
+            _incorrectAnswers  = newValue
+        }
     }
 }
